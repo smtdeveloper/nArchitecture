9f38e54441825e171abe119e5d8949cb5530c299
Samet Akca <developer2@cevizsoft.com>
2022-08-22 23:50:55 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Brand:Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id, string name):this()
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities; 
+using Persistence.Contexts;
+
+namespace Persistence.Repositories
+{
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+        
+    }
+}
