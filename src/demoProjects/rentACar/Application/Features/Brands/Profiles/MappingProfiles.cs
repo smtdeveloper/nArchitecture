99dcd3233ccb461b9195c02f031b09a3190b71dd
Samet Akca <developer2@cevizsoft.com>
2022-08-24 14:41:31 +0300

    Day2 Add Business rules & Global Expention Handle

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+
+        }
+    }
+}
