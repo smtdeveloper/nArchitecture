60dd89e38676e9ac8bcdb97857394fbcd883f0a3
Samet Akca <developer2@cevizsoft.com>
2022-09-03 02:24:19 +0300

    GetListBrand - GetByIdBrand - CreateValidator

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetListBrand
+{
+    public class GetListBrandQuery : IRequest<BrandListModel>
+    {
+        public PageRequest pageRequest { get; set; }
+        public class GetListBrandHandler : IRequestHandler<GetListBrandQuery, BrandListModel>
+        {
+           private readonly IBrandRepository _brandRepository;
+           private readonly IMapper _mapper;
+
+            public GetListBrandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<BrandListModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Brand> brands = await _brandRepository.GetListAsync(index: request.pageRequest.Page, size: request.pageRequest.PageSize);
+                
+
+                BrandListModel mappedBrandListModel = _mapper.Map<BrandListModel>(brands);
+
+                return mappedBrandListModel;
+            }
+        }
+
+    }
+}
--- src/demoProjects/rentACar/WebAPI/Controllers/BrandController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandController.cs
@@ -1,5 +1,9 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
+using Application.Features.Brands.Queries.GetByIdBrand;
+using Application.Features.Brands.Queries.GetListBrand;
+using Core.Application.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,5 +19,22 @@ namespace WebAPI.Controllers
             CreatedBrandDto result = await Mediator.Send(createBrandCommand);
             return Created("", result);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListBrand([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { pageRequest = pageRequest };
+            BrandListModel result = await Mediator.Send(getListBrandQuery);
+            return Ok(result);
+
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdIdBrandQuery)
+        {
+            BrandGetByIdDto brandGetByIdDto = await Mediator.Send(getByIdIdBrandQuery);
+            return Ok(brandGetByIdDto);
+        }
+
     }
 }
